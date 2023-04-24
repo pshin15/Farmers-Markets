9d9ee728815e03a7a7513d31de3a05926a8125b7
Paula <72952547+pshin15@users.noreply.github.com>
2023-04-23 23:50:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,31 +19,3 @@ document.addEventListener('DOMContentLoaded', async () => {
   fetchData();
 });
 
-
-// 2. processing request change your data into the map and list
-//function filterList()
-
-// assume data is an array of inspection data objects
-/*const filteredData = data.filter((item) => item.inspection_score !== null && item.latitude !== null && item.longitude !== null);
-
-const processedData = filteredData.map((item) => {
-return {
-latitude: item.latitude,
-longitude: item.longitude,
-score: item.inspection_score
-}
-});
-
-
-*/
-
-
-
-/*For the heat map, I will use array methods to filter and process the data from the API. 
-Specifically, I will use the filter() method to remove any data that does not have an inspection score, 
-and the map() method to extract the necessary information, such as latitude, longitude, and inspection score. 
-Then, I will use this filtered and processed data to display the heatmap using the leaflet.js library.
-*/
-
-
-//A quick filter that will return something based on a matching input 
