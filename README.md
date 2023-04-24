d9cfba246a806dd873122eae2439b96bddf16175
Paula Shin <paulashin15@gmail.com>
2023-04-23 23:42:05 -0400

    updated data request

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Food-Inspections
+
+This project will show a map of PG County, and the inspection scores of various food service facilities in the area. The goal is to help people to make better informed decisions when looking for a place to eat. 
--- script.js
+++ script.js
@@ -2,19 +2,48 @@ async function fetchData() {
 
   const mainForm = document.querySelector('.main_form'); 
 
-  //let currentList = [];
-
-  mainForm.addEventListener('Submit', async (SubmitEvent) => {
-    submitEvent.preventDefault();
+  mainForm.addEventListener('submit', async (Submit) => {
+    Submit.preventDefault();
     console.log('submitted form');
 
   // 1. asynch data request
   const response = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-  currentList = await results.json();
+  currentList = await response.json();
   console.table(currentList); 
-
-
   });
 }
 
-document.addEventListener('DOMContentLoaded', async () => fetchData()); // the async keyword means we can make API requests
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+  fetchData();
+});
+
+
+// 2. processing request change your data into the map and list
+//function filterList()
+
+// assume data is an array of inspection data objects
+/*const filteredData = data.filter((item) => item.inspection_score !== null && item.latitude !== null && item.longitude !== null);
+
+const processedData = filteredData.map((item) => {
+return {
+latitude: item.latitude,
+longitude: item.longitude,
+score: item.inspection_score
+}
+});
+
+
+*/
+
+
+
+/*For the heat map, I will use array methods to filter and process the data from the API. 
+Specifically, I will use the filter() method to remove any data that does not have an inspection score, 
+and the map() method to extract the necessary information, such as latitude, longitude, and inspection score. 
+Then, I will use this filtered and processed data to display the heatmap using the leaflet.js library.
+*/
+
+
+//A quick filter that will return something based on a matching input 
