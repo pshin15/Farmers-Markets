4ed43c6085a1bea4dce6926730996575d79b725a
Paula Shin <paulashin15@gmail.com>
2023-04-27 15:04:54 -0400

    load entire list of markets

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
 
-// 2. map array method to change my data into the map
-/* 
-  use array methods to filter and process the data from the API. 
-  use the filter() method to remove any data that does not have an inspection score
-*/
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function injectHTML(list) {
+  console.log('fired injectHTML')
+  const target = document.querySelector('#markets_list');
+  target.innerHTML = '';
+  list.forEach((item) => {
+    const str = `<li>${item.market_name}</li>`;
+    target.innerHTML += str
+  })
+}
+
+
 
 function initMap() {
-  const map = L.map('map').setView([38.99, -76.94], 13);
+  var map = L.map('map').setView([38.99, -76.94], 13);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 25,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
   return map;
 }
 
-async function findMarket() {
+/*
+function markerPlace(array, map) {
+  //console.log('array for markers', array);
+  array.forEach((item) => {
+    const {latitude, longitude} = item.location;
+    //L.marker([38.78, -77.01]).addTo(map);
+    L.marker([latitude, longitude]).addTo(map);
 
+    //marker.bindPopup(`<h3>${item.market_name}</h3><p>${item.location_address}</p>`);
+  });
+}
+*/
+
+
+
+async function findMarket() {
   const address = document.querySelector('.markets'); 
+  //const submitButton = document.querySelector('#data_load');
+
+  let marketList = [];
 
   address.addEventListener('submit', async (Submit) => {
     Submit.preventDefault();
     console.log('submitted form'); 
 
-    let currentList = [];
-
   // 1. asynch data request
     const response = await fetch(
       'https://data.princegeorgescountymd.gov/resource/sphi-rwax.json'); 
-    marketList = await response.json();  
+    marketList = await response.json();  //object from json data
     console.table(marketList);    
-    
-  initMap();
+    injectHTML(marketList);
   });
 }
 
-document.addEventListener('DOMContentLoaded', async () => 
-  initMap(), findMarket());
+const map = initMap();
+
+//markerPlace(marketList, map);
+
+
+document.addEventListener('DOMContentLoaded', async () => findMarket());
