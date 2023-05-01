feba7293c06e0916acf9f671aa43d23b2666abfc
Paula Shin <paulashin15@gmail.com>
2023-05-01 02:20:05 -0400

    im struggling here

--- script.js
+++ script.js
@@ -1,50 +1,4 @@
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
-
-
-function injectHTML(list, distance) {
-  console.log('fired injectHTML')
-  const target = document.querySelector('#markets_list');
-  target.innerHTML = '';
-  /*
-  if (Array.isArray(list)) {
-    list.forEach((item) => {
-      const str = `<li>${item.market_name} (Distance: ${item} miles)</li>`;
-      target.innerHTML += str;
-    });
-  } else {
-    const str = `<li>Distance: ${data} miles</li>`;
-    target.innerHTML = str;
-  }
-  */
-
-  list.forEach((item, index) => {
-    const str = `<li>${item.market_name} (${item.marketdistancemiles} miles)</li>`;
-    target.innerHTML += str
-  })
-
-}
-
-
-
-/*
-function haversine_distance(mk1, mk2) {
-  var R = 3958.8; // Radius of the Earth in miles
-  var rlat1 = mk1.position.lat() * (Math.PI/180); // Convert degrees to radians
-  var rlat2 = mk2.position.lat() * (Math.PI/180); // Convert degrees to radians
-  var difflat = rlat2-rlat1; // Radian difference (latitudes)
-  var difflon = (mk2.position.lng()-mk1.position.lng()) * (Math.PI/180); // Radian difference (longitudes)
-
-  var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
-  return d;
-}
-*/
-
-
 // finds the users current coordinates  
 function getCurrentLocation() {
   if (navigator.geolocation) {
@@ -56,6 +10,7 @@ function getCurrentLocation() {
     console.log("Geolocation is not supported by this browser.");
   }
 }
+
 
 
 let userLatLng;
@@ -70,22 +25,31 @@ function calculateDistance(marketList) {
     console.log(marketdistancemiles);
 
   });
+};
 
-  //injectHTML(marketdistance);
 
-/*
-  // 4. Sort list of farmers markets by distance
-  marketList.sort((a, b) => a.distance - b.distance);
 
-  // 5. Display sorted list of farmers markets
+function injectHTML(marketList) {
+  console.log('fired injectHTML')
   const target = document.querySelector('#markets_list');
   target.innerHTML = '';
-  marketList.forEach((item, index) => {
-    const str = `<li>${item.market_name} (${item.marketdistance} miles)</li>`;
-    target.innerHTML += str;
-  });
+  /*
+  if (Array.isArray(list)) {
+    list.forEach((item) => {
+      const str = `<li>${item.market_name} (${item.marketdistancemiles} miles away)</li>`;
+      target.innerHTML += str;
+    });
+  } else {
+    const str = `<li>Distance: ${data} miles</li>`;
+    target.innerHTML = str;
+  }
   */
-};
+  marketList.forEach((item, index) => {
+    const str = `<li>${item.market_name} (${item.marketdistancemiles} miles)</li>`;
+    target.innerHTML += str
+  })
+  
+}
 
 
 
